2952612d08ca738557b6630839e968a818f63eb1
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-06 09:24:30 +0300

    print review method

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,12 @@
 class Review < ActiveRecord::Base 
+    belongs_to :user
     belongs_to :product
+
+    def print_review
+        "Review for #{self.product.name} by #{self.user}: #{self.star_rating}. #{comment}"
+
+    end
 end
 
 
-    # A Review belongs to a Product, and a Review also belongs to a User. 
+# Review for {insert product name} by {insert user name}: {insert review star_rating}. {insert review comment}
