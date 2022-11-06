a6c90bd5b7b4ef7fcbe564df47a081b5add4744f
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-06 16:31:57 +0300

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,17 @@ product3 = Product.create(name: "Dry Erase Markers", price: 5)
 product4 = Product.create(name: "Ballpoint Pens", price: 2)
 product5 = Product.create(name: "Scotch Tape", price: 3)
 
-puts "Creating reviews..."
-# *****************************************************************
+
 # * TODO: create reviews! Remember, a review belongs to a product *
-# * and a review belongs to an user.                              *
-# *****************************************************************
-# Create Reviews Here
+# * and a review belongs to an user.   
+puts "Creating reviews..."
+Review.create(star_rating: 6, comment: "Descent", product_id: product1.id, user_id: user1.id)
+Review.create(star_rating: 1, comment: "Worse", product_id: product1.id, user_id: user1.id)
+Review.create(star_rating: 5, comment: "OK", product_id: product3.id, user_id: user2.id)
+Review.create(star_rating: 8, comment: "Really Good", product_id: product2.id, user_id: user3.id)
+Review.create(star_rating: 4, comment: "Slightly below average", product_id: product5.id, user_id: user2.id)
+Review.create(star_rating: 7, comment: "It's alright, but not the best pen", product_id: product4.id, user_id: user3.id)
+Review.create(star_rating: 9, comment: "Loved it", product_id: product3.id, user_id: user2.id)
+
 
 puts "Seeding done!"
