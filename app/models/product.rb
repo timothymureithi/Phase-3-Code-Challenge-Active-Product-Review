542a57544f8ae042f3ef360a7b60e064ddb8dc82
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-06 18:00:05 +0300

    averae rating method updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
     has_many :reviews
 
     def leave_review(user, star_rating, comment)
-        Review.create(product: self, user:user, star_rating: star_rating, comment: comment)
+        Review.create(star_rating: star_rating, comment: comment, product_id: self.id, user_id: user.id)
     end
 
     def print_all_reviews
@@ -12,7 +12,10 @@ class Product < ActiveRecord::Base
         end
    end
 
-   
+   def average_rating
+    self.reviews.average(:star_rating).to_f
+   end
+
 
 end
 
@@ -20,3 +23,6 @@ end
 
 # takes a `User` (an instance of the `User` class), a `star_rating` (integer), and a `comment` (string) as arguments, 
 # and creates a new `Review` in the database associated with this Product and the User
+
+# `Product#average_rating`
+# - returns a `float` representing the average star rating for all reviews for this product
