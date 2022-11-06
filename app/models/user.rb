3d51f1468f436e5bd5a790e373e2922ccc1fcfd4
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-06 18:01:44 +0300

    completed code-challenge

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ActiveRecord::Base
-    has_many: :products
-    has_many: :reviews
+    has_many :products, through: :reviews
+    has_many :reviews
+
+    def favorite_product
+        self.reviews.order(:star_rating).last.product
+    end
+
+    def remove_reviews(product)
+        self.reviews.where(product: product).destroy_all
+    end
+
 end
+
+    
+# User#reviews
+# returns a collection of all the Reviews that the User has given
+# User#products
+# returns a collection of all the Products that the User has reviewed
